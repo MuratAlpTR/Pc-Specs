da7a6a3d4256f82245bc446a5d43220fb4c7d238
Murat Alp <59587470+MuratAlpTR@users.noreply.github.com>
2021-11-29 18:54:37 +0300

    Add files via upload

---
+++ PC-Specs_EN.py
@@ -0,0 +1,42 @@
+# Developed by MuratAlpTR project...
+# Github Link : https://github.com/MuratAlpTR
+
+
+import platform
+
+            # 1 = Getting the computer's connection name
+print(platform.node())
+
+            # 2 = Operating system learning
+print(platform.system())
+
+            # 3 = Getting processor information
+print(platform.processor())
+
+            # 4 = Get generation feature information
+print(platform.release())
+
+            # 5 = Getting Architectural Information
+print(platform.architecture())
+
+            # 6 = Getting machine information
+print(platform.machine())
+
+            # 7 = Computer portable interface
+print(platform.uname())
+
+            # 8 = Getting the system release version
+print(platform.version())
+
+
+####      PYTHON      ####
+
+            # 9 = Getting the Python version
+print(platform.python_branch())
+
+            # 10 = Getting the Python implementation
+
+print(platform.python_implementation())
+
+            # 11 = learn python version version
+print(platform.python_version())
---
+++ PC-Specs_TR.py
@@ -0,0 +1,41 @@
+# MuratAlpTR Proje tarafından geliştirilmiştir...
+# Github Link : https://github.com/MuratAlpTR
+
+
+import platform
+
+            # 1 = Bilgisayarın bağlantı adı alma
+print(platform.node())
+
+            # 2 = işletim sistemini öğrenme
+print(platform.system())
+
+            # 3 = İşlemci özellik bilgisi alma
+print(platform.processor())
+
+            # 4 = Nesil özellik bilgisi alma
+print(platform.release())
+
+            # 5 = Mimari Bilgisini almak
+print(platform.architecture())
+
+            # 6 = Makine bilgisini alma
+print(platform.machine())
+
+            # 7 = Bilgisayarı taşınabilir arayüzü
+print(platform.uname())
+
+            # 8 = Sisyem yayın sürümünü alma
+print(platform.version())
+
+
+####      PYTHON      ####
+
+            # 9 = Python verisyonunu alma
+print(platform.python_branch())
+
+            # 10 = Python implementasyonun alma
+print(platform.python_implementation())
+
+            # 11 = Python verisyonun sürüm öğrenmek
+print(platform.python_version())
